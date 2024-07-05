9566800684d70f1c145aac4067a3d311218be198
ErickGomezUCA <00300723@uca.edu.sv>
2024-07-05 11:38:41 -0600

    Crear clase Tarjeta

---
+++ parcial-final/src/main/java/org/example/parcialfinal/backend/Tarjeta.java
@@ -0,0 +1,47 @@
+package org.example.parcialfinal.backend;
+
+public class Tarjeta {
+    private int id;
+    private String numeroTarjeta;
+    private String fechaExpiracion;
+    private String tipo;
+
+    public Tarjeta(int id, String numeroTarjeta, String fechaExpiracion, String tipo) {
+        this.id = id;
+        this.numeroTarjeta = numeroTarjeta;
+        this.fechaExpiracion = fechaExpiracion;
+        this.tipo = tipo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNumeroTarjeta() {
+        return numeroTarjeta;
+    }
+
+    public void setNumeroTarjeta(String numeroTarjeta) {
+        this.numeroTarjeta = numeroTarjeta;
+    }
+
+    public String getFechaExpiracion() {
+        return fechaExpiracion;
+    }
+
+    public void setFechaExpiracion(String fechaExpiracion) {
+        this.fechaExpiracion = fechaExpiracion;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+}
