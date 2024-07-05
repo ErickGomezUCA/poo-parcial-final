7c4f8dd0f681b94f1a2113d1cf8ce9bde59231ec
ErickGomezUCA <00300723@uca.edu.sv>
2024-07-05 11:39:44 -0600

    Crear clase Compra

---
+++ parcial-final/src/main/java/org/example/parcialfinal/backend/Compra.java
@@ -0,0 +1,57 @@
+package org.example.parcialfinal.backend;
+
+public class Compra {
+    private int id;
+    private String fechaCompra;
+    private double monto;
+    private String descripcion;
+    private int idTajerta;
+
+    public Compra(int id, String fechaCompra, double monto, String descripcion, int idTajerta) {
+        this.id = id;
+        this.fechaCompra = fechaCompra;
+        this.monto = monto;
+        this.descripcion = descripcion;
+        this.idTajerta = idTajerta;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFechaCompra() {
+        return fechaCompra;
+    }
+
+    public void setFechaCompra(String fechaCompra) {
+        this.fechaCompra = fechaCompra;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+
+    public void setMonto(double monto) {
+        this.monto = monto;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getIdTajerta() {
+        return idTajerta;
+    }
+
+    public void setIdTajerta(int idTajerta) {
+        this.idTajerta = idTajerta;
+    }
+}
