40f1f70eb3a5724cb21d6b5f0471b54104894b44
ErickGomezUCA <00300723@uca.edu.sv>
2024-07-05 19:03:19 -0600

    Agregar controller para Tarjeta

--- parcial-final/src/main/java/module-info.java
+++ parcial-final/src/main/java/module-info.java
@@ -5,4 +5,8 @@ module org.example.parcialfinal {
 
     opens org.example.parcialfinal to javafx.fxml;
     exports org.example.parcialfinal;
+    exports org.example.parcialfinal.applications;
+    opens org.example.parcialfinal.applications to javafx.fxml;
+    exports org.example.parcialfinal.controllers;
+    opens org.example.parcialfinal.controllers to javafx.fxml;
 }
--- parcial-final/src/main/java/org/example/parcialfinal/HelloController.java
+++ parcial-final/src/main/java/org/example/parcialfinal/HelloController.java
@@ -1,4 +1,4 @@
-package org.example.parcialfinal;
+package org.example.parcialfinal.controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
