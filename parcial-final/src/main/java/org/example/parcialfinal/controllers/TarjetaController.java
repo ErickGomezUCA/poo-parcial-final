2e50dbf170ed1dd4dec13c47da0985b83d250cfd
ErickGomezUCA <00300723@uca.edu.sv>
2024-07-07 11:45:12 -0600

    Mover tarjeta.fxml a carpeta applications

--- parcial-final/src/main/java/org/example/parcialfinal/controllers/TarjetaController.java
+++ parcial-final/src/main/java/org/example/parcialfinal/controllers/TarjetaController.java
@@ -1,15 +1,25 @@
 package org.example.parcialfinal.controllers;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
+import org.example.parcialfinal.backend.database.DBConnection;
+
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class TarjetaController {
 
-    @FXML
-    private TextField idTarjetaFechaExp;
+    DBConnection connection = DBConnection.getInstance();
 
     @FXML
-    private TextField txtTajertaNum;
+    private DatePicker txtTarjetaFechaExp;
+
+    @FXML
+    private TextField txtTarjetaNum;
 
     @FXML
     private TextField txtTarjetaId;
@@ -29,7 +39,19 @@ public class TarjetaController {
 
     @FXML
     void btnCrearTarjetaClick(ActionEvent event) {
+        try {
+            PreparedStatement ps = connection.getConnection().prepareStatement("INSERT INTO Tarjeta(num_tarjeta, fecha_expiracion, tipo_tarjeta) VALUES(?, ?, ?)");
+            ps.setString(1 , txtTarjetaNum.getText());
 
+            Date sqlDate = Date.valueOf(txtTarjetaFechaExp.getValue());
+            ps.setDate(2 , sqlDate);
+
+            ps.setString(3 , txtTarjetaTipo.getText());
+            ps.executeUpdate();
+            System.out.println("Tarjeta creada en el sistema");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @FXML
