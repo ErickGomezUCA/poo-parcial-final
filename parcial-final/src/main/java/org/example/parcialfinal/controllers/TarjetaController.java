585855a011e861bd35ea06747bbaab5f58d698aa
ErickGomezUCA <00300723@uca.edu.sv>
2024-07-07 12:55:33 -0600

    Seleccionar Faciliadores desde el input selectFacilitador

--- parcial-final/src/main/java/org/example/parcialfinal/controllers/TarjetaController.java
+++ parcial-final/src/main/java/org/example/parcialfinal/controllers/TarjetaController.java
@@ -8,10 +8,13 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
+import org.example.parcialfinal.backend.Facilitador;
 import org.example.parcialfinal.backend.database.DBConnection;
 
 import java.net.URL;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class TarjetaController implements Initializable {
@@ -30,10 +33,18 @@ public class TarjetaController implements Initializable {
     @FXML
     private ComboBox<String> selectTarjetaTipo;
 
+    @FXML
+    private TextField txtIdCliente;
+
+    @FXML
+    private ComboBox<Facilitador> selectFacilitador;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         ObservableList<String> tipoTarjetaValues = FXCollections.observableArrayList("Credito", "Debito");
         selectTarjetaTipo.setItems(tipoTarjetaValues);
+        ObservableList<Facilitador> facilitadoresValues = FXCollections.observableArrayList(obtenerFacilitadores());
+        selectFacilitador.setItems(facilitadoresValues);
     }
 
     @FXML
@@ -63,7 +74,6 @@ public class TarjetaController implements Initializable {
                 + "\nfecha de expiracion: " + rs.getString("fecha_expiracion")
                 + "\ntipo de tarjeta: " + rs.getString("tipo_tarjeta") + "\n");
             }
-
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -71,13 +81,28 @@ public class TarjetaController implements Initializable {
 
     @FXML
     void btnCrearTarjetaClick(ActionEvent event) {
+        int lastIdInserted = 0;
+
         try {
-            PreparedStatement ps = connection.getConnection().prepareStatement("INSERT INTO Tarjeta(num_tarjeta, fecha_expiracion, tipo_tarjeta) VALUES(?, ?, ?)");
-            ps.setString(1 , txtTarjetaNum.getText());
-            ps.setDate(2 , Date.valueOf(dateFechaExp.getValue()));
-            ps.setString(3 , selectTarjetaTipo.getValue());
-            ps.executeUpdate();
+            PreparedStatement psTarjeta = connection.getConnection().prepareStatement("INSERT INTO Tarjeta(num_tarjeta, fecha_expiracion, tipo_tarjeta) VALUES(?, ?, ?)");
+            psTarjeta.setString(1 , txtTarjetaNum.getText());
+            psTarjeta.setDate(2 , Date.valueOf(dateFechaExp.getValue()));
+            psTarjeta.setString(3 , selectTarjetaTipo.getValue());
+            psTarjeta.executeUpdate();
             System.out.println("Tarjeta creada en el sistema");
+
+            // Obtener ultimo ID del autoincrement
+            ResultSet rs = connection.getConnection().createStatement().executeQuery("SELECT LAST_INSERT_ID() FROM Tarjeta");
+            while (rs.next()) {
+                lastIdInserted = rs.getInt(1);
+            }
+
+            PreparedStatement psComprasInteligentes = connection.getConnection().prepareStatement("INSERT INTO Compras_Inteligentes(id_tarjeta_CI, id_cliente_CI, id_facilitador_CI) VALUES(?, ?, ?,)");
+            psComprasInteligentes.setInt(1, lastIdInserted);
+            psComprasInteligentes.setInt(2, Integer.parseInt(txtIdCliente.getText()));
+            psComprasInteligentes.setInt(3, selectFacilitador.getValue().getId());
+            psComprasInteligentes.executeUpdate();
+            System.out.println("Compras inteligentes registrada");
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -95,4 +120,18 @@ public class TarjetaController implements Initializable {
         }
     }
 
+
+    private List<Facilitador> obtenerFacilitadores() {
+        List<Facilitador> facilitadores = new ArrayList<>();
+        try {
+            ResultSet rs = connection.getConnection().createStatement().executeQuery("SELECT * FROM Facilitador");
+            while (rs.next()) {
+                facilitadores.add(new Facilitador(rs.getInt("id"), rs.getString("facilitador")));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return facilitadores;
+    }
 }
