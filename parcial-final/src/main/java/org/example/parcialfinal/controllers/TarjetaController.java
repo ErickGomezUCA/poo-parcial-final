2174b8ffce8f6007c9ab86c1c56051c3f83b7d73
ErickGomezUCA <00300723@uca.edu.sv>
2024-07-07 11:55:03 -0600

    Buscar Tarjetas en CRUD

--- parcial-final/src/main/java/org/example/parcialfinal/controllers/TarjetaController.java
+++ parcial-final/src/main/java/org/example/parcialfinal/controllers/TarjetaController.java
@@ -5,9 +5,7 @@ import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import org.example.parcialfinal.backend.database.DBConnection;
 
-import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
@@ -34,7 +32,20 @@ public class TarjetaController {
 
     @FXML
     void btnBuscarTarjetaClick(ActionEvent event) {
+        try {
+            Statement stmt = connection.getConnection().createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM Tarjeta WHERE id = " + txtTarjetaId.getText() + ";");
 
+            while (rs.next()) {
+                System.out.println("id: " + rs.getString("id")
+                + "\nnumero de tarjeta: " + rs.getString("num_tarjeta")
+                + "\nfecha de expiracion: " + rs.getString("fecha_expiracion")
+                + "\ntipo de tarjeta: " + rs.getString("tipo_tarjeta") + "\n");
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @FXML
