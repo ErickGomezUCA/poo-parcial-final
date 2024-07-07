341d7bfa19d1827997167dbbe876a53118312d49
ErickGomezUCA <00300723@uca.edu.sv>
2024-07-07 16:49:00 -0600

    Establecer valores de facilitadores en reporte D

--- parcial-final/src/main/java/org/example/parcialfinal/controllers/ReporteDController.java
+++ parcial-final/src/main/java/org/example/parcialfinal/controllers/ReporteDController.java
@@ -1,4 +1,30 @@
 package org.example.parcialfinal.controllers;
 
-public class ReporteDController {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import org.example.parcialfinal.backend.Facilitador;
+import org.example.parcialfinal.backend.database.DatabaseUtils;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ReporteDController implements Initializable {
+
+    @FXML
+    private ComboBox<Facilitador> selectFacilitador;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        ObservableList<Facilitador> facilitadorValues = FXCollections.observableArrayList(DatabaseUtils.obtenerFacilitadores());
+        selectFacilitador.setItems(facilitadorValues);
+    }
+
+    @FXML
+    void generarReporteD(ActionEvent event) {
+
+    }
 }
