48ad7edaebde206dde288d3c0a90663e9f3d0ce7
ErickGomezUCA <00300723@uca.edu.sv>
2024-07-07 19:12:25 -0600

    Mostrar lista de clientes para reporte C

--- parcial-final/src/main/java/org/example/parcialfinal/controllers/ReporteCController.java
+++ parcial-final/src/main/java/org/example/parcialfinal/controllers/ReporteCController.java
@@ -1,14 +1,33 @@
 package org.example.parcialfinal.controllers;
 
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import org.example.parcialfinal.backend.Cliente;
+import org.example.parcialfinal.backend.database.DBConnection;
+import org.example.parcialfinal.backend.database.DatabaseUtils;
 
-public class ReporteCController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ReporteCController implements Initializable {
 
     @FXML
     private ComboBox<Cliente> selectCliente;
+
+    DBConnection connection;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        connection = DBConnection.getInstance();
+
+        ObservableList<Cliente> clienteValues = FXCollections.observableArrayList(DatabaseUtils.obtenerClientes());
+        selectCliente.setItems(clienteValues);
+    }
 
     @FXML
     void generarReporteC(ActionEvent event) {
