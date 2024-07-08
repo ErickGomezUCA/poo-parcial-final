0e127b48fb6611e56a31da2cc703def4fd69c214
ErickGomezUCA <00300723@uca.edu.sv>
2024-07-07 19:08:05 -0600

    Obtener lista de clientes en la base de datos

--- parcial-final/src/main/java/org/example/parcialfinal/backend/database/DatabaseUtils.java
+++ parcial-final/src/main/java/org/example/parcialfinal/backend/database/DatabaseUtils.java
@@ -1,5 +1,6 @@
 package org.example.parcialfinal.backend.database;
 
+import org.example.parcialfinal.backend.Cliente;
 import org.example.parcialfinal.backend.Facilitador;
 
 import java.sql.ResultSet;
@@ -22,5 +23,19 @@ public class DatabaseUtils {
         }
 
         return facilitadores;
+    }
+
+    public static List<Cliente> obtenerClientes() {
+        List<Cliente> clientes = new ArrayList<>();
+        try {
+            ResultSet rs = connection.getConnection().createStatement().executeQuery("SELECT * FROM Cliente");
+            while (rs.next()) {
+                clientes.add(new Cliente(rs.getInt("id"), rs.getString("nombre_completo"), rs.getString("direccion"), rs.getString("telefono")));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return clientes;
     }
 }
