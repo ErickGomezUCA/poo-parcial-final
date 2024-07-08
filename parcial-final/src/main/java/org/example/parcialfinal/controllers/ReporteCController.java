20a6eae128c43e8e2f3cb59529c84dcd3f3cb66b
ErickGomezUCA <00300723@uca.edu.sv>
2024-07-07 19:05:25 -0600

    Crear diseño para Reporte C

---
+++ parcial-final/src/main/java/org/example/parcialfinal/controllers/ReporteCController.java
@@ -0,0 +1,19 @@
+package org.example.parcialfinal.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import org.example.parcialfinal.backend.Cliente;
+
+public class ReporteCController {
+
+    @FXML
+    private ComboBox<Cliente> selectCliente;
+
+    @FXML
+    void generarReporteC(ActionEvent event) {
+
+    }
+
+}
+
