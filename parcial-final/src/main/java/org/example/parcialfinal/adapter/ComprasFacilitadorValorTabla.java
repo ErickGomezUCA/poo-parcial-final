054bb9c681415dc81fd5146ac7ccb0440dc7fa00
ErickGomezUCA <00300723@uca.edu.sv>
2024-07-09 13:29:29 -0600

    Agregar obtener tarjetas

---
+++ parcial-final/src/main/java/org/example/parcialfinal/adapter/ComprasFacilitadorValorTabla.java
@@ -0,0 +1,10 @@
+package org.example.parcialfinal.adapter;
+
+public interface ComprasFacilitadorValorTabla {
+    int obtenerIdCliente();
+    String obtenerNombre();
+    String obtenerNumTarjeta();
+    String obtenerFacilitador();
+    int obtenerCantidad();
+    double obtenerMontoTotal();
+}
