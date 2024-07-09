271ce452a3f1ae7624d9003ad372dee18e0bfb7d
Paolalopez4 <00167523@uca.edu.sv>
2024-07-09 00:22:37 -0600

    Anadir implementacion para txt y cambios a reportes A y B

--- parcial-final/src/main/java/org/example/parcialfinal/controllers/ReporteAController.java
+++ parcial-final/src/main/java/org/example/parcialfinal/controllers/ReporteAController.java
@@ -1,82 +1,48 @@
 package org.example.parcialfinal.controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import org.example.parcialfinal.backend.Cliente;
 import org.example.parcialfinal.backend.database.DBConnection;
+import org.example.parcialfinal.backend.database.DatabaseUtils;
+import org.example.parcialfinal.reports.ReporteA;
+import org.example.parcialfinal.reports.ReporteC;
 
+import java.net.URL;
 import java.sql.*;
+import java.util.ResourceBundle;
 
-public class ReporteAController {
-    DBConnection connection = DBConnection.getInstance();
-
-    @FXML
-    private TextField txtIdCliente;
+public class ReporteAController implements Initializable {
 
     @FXML
-    private TextField txtFechaDesde;
+    private ComboBox<Cliente> selectCliente;
 
     @FXML
-    private TextField txtFechaHasta;
-
-
-    @FXML
-    private TextArea txtMensajeReporteA;
+    private DatePicker fechaInicio;
 
     @FXML
-    public void generarReporteA() {
-        txtFechaDesde.clear();
+    private DatePicker fechaFin;
 
-        int idCliente = Integer.parseInt(txtIdCliente.getText());
-        String fechaDesde = txtFechaDesde.getText();
-        String fechaHasta = txtFechaHasta.getText();
+    DBConnection connection;
 
-        if (txtIdCliente.getText().equals("")) {
-            txtIdCliente.setText("Este campo es obligatorio");
-            return;
-        }
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        connection = DBConnection.getInstance();
 
-        if (txtFechaDesde.getText().equals("")) {
-            txtFechaDesde.setText("Este campo es obligatorio");
-            return;
-        }
+        ObservableList<Cliente> clienteValues = FXCollections.observableArrayList(DatabaseUtils.obtenerClientes());
+        selectCliente.setItems(clienteValues);
+    }
 
-        if (txtFechaHasta.getText().equals("")) {
-            txtFechaHasta.setText("Este campo es obligatorio");
-            return;
-        }
-
-        try {
-            PreparedStatement st = connection.getConnection().prepareStatement("SELECT clnt.nombre_completo, cmpr.fecha_compra, cmpr.monto, cmpr.descripcion, t.num_tarjeta " +
-                                                                                   "FROM Cliente clnt " +
-                                                                                   "INNER JOIN Compras_Inteligentes ci " +
-                                                                                   "ON clnt.id = ci.id_cliente_CI " +
-                                                                                   "INNER JOIN Tarjeta t " +
-                                                                                   "ON ci.id_tarjeta_CI = t.id " +
-                                                                                   "INNER JOIN Compra cmpr " +
-                                                                                   "ON t.id = cmpr.id_tarjeta_C " +
-                                                                                   "WHERE clnt.id = ? " +
-                                                                                   "AND cmpr.fecha_compra " +
-                                                                                   "BETWEEN ? AND ? ");
-
-            st.setInt(1, idCliente);
-            st.setString(2, fechaDesde);
-            st.setString(3, fechaHasta);
-            ResultSet rs = st.executeQuery();
-
-            while (rs.next()) {
-                String nombreCompleto = rs.getString("nombre_completo");
-                //todo cambiar a date
-                String fechaCompra = rs.getString("fecha_compra");
-                Double monto = rs.getDouble("monto");
-                String descripcion = rs.getString("descripcion");
-                String numTarjeta = rs.getString("num_tarjeta");
-
-                txtMensajeReporteA.appendText("COMPRA(S) ENCONTRADA(S):\n" + nombreCompleto + ", " + fechaCompra + ", " + monto + ", " + descripcion + ", " + numTarjeta + "\n");
-            }
-            connection.closeConnection();
-        } catch (SQLException e) {
-            txtMensajeReporteA.setText("Error: " + e.getMessage());
-        }
+    @FXML
+    void generarReporteA(ActionEvent event) {
+        ReporteA reporteA = new ReporteA();
+        reporteA.generarReporte(selectCliente, fechaInicio, fechaFin);
     }
 }
--- parcial-final/src/main/java/org/example/parcialfinal/controllers/ReporteBController.java
+++ parcial-final/src/main/java/org/example/parcialfinal/controllers/ReporteBController.java
@@ -1,19 +1,29 @@
 package org.example.parcialfinal.controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import org.example.parcialfinal.backend.Cliente;
 import org.example.parcialfinal.backend.database.DBConnection;
+import org.example.parcialfinal.backend.database.DatabaseUtils;
+import org.example.parcialfinal.reports.ReporteA;
+import org.example.parcialfinal.reports.ReporteB;
 
+import java.net.URL;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ResourceBundle;
 
-public class ReporteBController {
-    DBConnection connection = DBConnection.getInstance();
+public class ReporteBController implements Initializable {
 
     @FXML
-    private TextField txtIdCliente;
+    private ComboBox<Cliente> selectCliente;
 
     @FXML
     private TextField txtMesABuscar;
@@ -21,58 +31,19 @@ public class ReporteBController {
     @FXML
     private TextField txtAnioABuscar;
 
-    @FXML
-    private TextArea txtMensajeReporteB;
+    DBConnection connection;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        connection = DBConnection.getInstance();
+
+        ObservableList<Cliente> clienteValues = FXCollections.observableArrayList(DatabaseUtils.obtenerClientes());
+        selectCliente.setItems(clienteValues);
+    }
 
     @FXML
-    public void generarReporteB() {
-        int idCliente = Integer.parseInt(txtIdCliente.getText());
-        int mesABuscar = Integer.parseInt(txtMesABuscar.getText());
-        int anioABuscar = Integer.parseInt(txtAnioABuscar.getText());
-
-        if (txtIdCliente.getText().equals("")) {
-            txtIdCliente.setText("Este campo es obligatorio");
-            return;
-        }
-
-        if (txtMesABuscar.getText().equals("")) {
-            txtMesABuscar.setText("Este campo es obligatorio");
-            return;
-        }
-
-        if (txtAnioABuscar.getText().equals("")) {
-            txtAnioABuscar.setText("Este campo es obligatorio");
-            return;
-        }
-
-        try {
-            PreparedStatement st = connection.getConnection().prepareStatement("SELECT c.nombre_completo, SUM(com.monto) AS monto_total " +
-                                                                                    "FROM Cliente c " +
-                                                                                    "JOIN Compras_Inteligentes ci " +
-                                                                                    "ON c.id = ci.id_cliente_CI " +
-                                                                                    "JOIN Tarjeta t " +
-                                                                                    "ON ci.id_tarjeta_CI = t.id " +
-                                                                                    "JOIN Compra com " +
-                                                                                    "ON t.id = com.id_tarjeta_C " +
-                                                                                    "WHERE c.id = ? " +
-                                                                                    "AND YEAR(com.fecha_compra) = ? " +
-                                                                                    "AND MONTH(com.fecha_compra) = ? " +
-                                                                                    "GROUP BY c.nombre_completo");
-
-                    st.setInt(1, idCliente);
-                    st.setInt(2, anioABuscar);
-                    st.setInt(3, mesABuscar);
-
-                    ResultSet rs = st.executeQuery();
-                    if (rs.next()) {
-                        String nombreCompleto = rs.getString("nombre_completo");
-                        double montoTotal = rs.getDouble("monto_total");
-
-                        txtMensajeReporteB.appendText("GASTO TOTAL DE CLIENTE:\n" + nombreCompleto + ", " + montoTotal + ".\n");
-                    }
-                    connection.closeConnection();
-        } catch (SQLException e) {
-            txtMensajeReporteB.setText("Error: " + e.getMessage());
-        }
+    void generarReporteB(ActionEvent event) {
+        ReporteB reporteB = new ReporteB();
+        reporteB.generarReporte(selectCliente, txtAnioABuscar, txtMesABuscar);
     }
 }
