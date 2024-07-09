e52b57d1165092eb26cec74aca080803e914bc45
ErickGomezUCA <00300723@uca.edu.sv>
2024-07-09 10:43:47 -0600

    Agregar ids y eventos para reporte C y D

--- parcial-final/src/main/java/org/example/parcialfinal/controllers/ReportesController.java
+++ parcial-final/src/main/java/org/example/parcialfinal/controllers/ReportesController.java
@@ -1,13 +1,39 @@
 package org.example.parcialfinal.controllers;
 
 import javafx.fxml.Initializable;
-
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ReportesController implements Initializable {
+    @FXML
+    private ComboBox<?> selectCliente_RC;
+
+    @FXML
+    private ComboBox<?> selectFacilitador_RD;
+
+    @FXML
+    private TableView<?> tbRes_RD;
+
+    @FXML
+    private TextArea txtAreaRes_RC;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    @FXML
+    void clickGenerarReporteC(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickGenerarReporteD(ActionEvent event) {
 
     }
 }
