23105c7afaaa8812660ba3c9b73db553f6f4812d
Valency012 <00090123@uca.edu.sv>
2024-07-09 21:09:24 -0600

    Agregando comentarios

--- parcial-final/src/main/java/org/example/parcialfinal/backend/Facilitador.java
+++ parcial-final/src/main/java/org/example/parcialfinal/backend/Facilitador.java
@@ -1,37 +1,38 @@
 package org.example.parcialfinal.backend;
 
 public class Facilitador {
-    private int id;
-    private String facilitador;
+    private int id; // 00090123 Identificador único del facilitador
+    private String facilitador; // 00090123 Nombre del facilitador
 
-    public Facilitador() {
-        id = 0;
-        facilitador = "";
+    public Facilitador() { // 00090123 Constructor por defecto de la clase Facilitador
+        id = 0; // 00090123 Inicializa el ID del facilitador como 0
+        facilitador = ""; // 00090123 Inicializa el nombre del facilitador como cadena vacía
     }
 
-    public Facilitador(int id, String facilitador) {
-        this.id = id;
-        this.facilitador = facilitador;
+    public Facilitador(int id, String facilitador) { // 00090123 Constructor con parámetros de la clase Facilitador
+        this.id = id; // 00090123 Asigna el ID del facilitador con el valor proporcionado
+        this.facilitador = facilitador; // 00090123 Asigna el nombre del facilitador con el valor proporcionado
     }
 
-    public int getId() {
+    public int getId() { // 00090123 Método para obtener el ID del facilitador
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(int id) { // 00090123 Método para establecer el ID del facilitador
         this.id = id;
     }
 
-    public String getFacilitador() {
+    public String getFacilitador() { // 00090123 Método para obtener el nombre del facilitador
         return facilitador;
     }
 
-    public void setFacilitador(String facilitador) {
+    public void setFacilitador(String facilitador) { // 00090123 Método para establecer el nombre del facilitador
         this.facilitador = facilitador;
     }
 
     @Override
-    public String toString() {
+    public String toString() { // 00090123 Método para obtener una representación en cadena del facilitador
         return facilitador;
     }
+
 }
