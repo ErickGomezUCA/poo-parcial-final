ab953249ec5e8ccec85ca23fc33cd5af83ac50bb
Valency012 <00090123@uca.edu.sv>
2024-07-09 20:36:37 -0600

    Agregando: comentando CompraApplication.fxml

--- parcial-final/src/main/java/org/example/parcialfinal/applications/CompraApplication.java
+++ parcial-final/src/main/java/org/example/parcialfinal/applications/CompraApplication.java
@@ -9,16 +9,16 @@ import java.io.IOException;
 
 public class CompraApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(CompraApplication.class.getResource("compra.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
-        stage.setTitle("Compra");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage stage) throws IOException { // 00090123 Método start requerido por la clase Application para iniciar la aplicación
+        FXMLLoader fxmlLoader = new FXMLLoader(CompraApplication.class.getResource("compra.fxml")); // 00090123 Carga el archivo FXML para la interfaz de usuario de la compra
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 720); // 00090123 Crea una escena con el contenido cargado desde el archivo FXML y establece el tamaño de la ventana
+        stage.setTitle("Compra"); // 00090123 Establece el título de la ventana
+        stage.setScene(scene); // 00090123 Establece la escena en el escenario (Stage)
+        stage.show(); // 00090123 Muestra la ventana
     }
 
-    public static void main(String[] args) {
-        launch();
+    public static void main(String[] args) { // 00090123 Método principal de inicio de la aplicación
+        launch(); // 00090123 Método estático de la clase Application que inicia la aplicación JavaFX
     }
 
 }
